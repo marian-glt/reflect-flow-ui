38d818be61ff331c3df7b00a0eca9c6e8ea40502
marian-glt <38663778+marian-glt@users.noreply.github.com>
2025-02-09 17:32:35 +0000

    start of the design for the homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen bg-gray-300`}
+				className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen bg-slate-600`}
 			>
 				{children}
 			</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-import Sidebar from "./components/Sidebar";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Home() {
+import Tabs from "./components/Tabs";
+
+export default function Home(): React.ReactNode {
 	return (
-		<>
-			<Sidebar />
-			<section className="col-span-3">Home</section>
-		</>
+		<div className="h-full grid grid-flow-row gap-1 grid-rows-12">
+			<div className="row-span-1 mb-1">
+				<div className="flex flex-col items-left pl-8 w-full h-full bg-slate-800/30 rounded-b-3xl shadow-slate-950/20 shadow-md">
+					<div className="flex flex-row w-52 justify-between my-auto">
+						<FontAwesomeIcon
+							icon={faBook}
+							className="w-8 h-8 mt-0"
+						/>
+						<h1 className="text-3xl font-medium font-geist-sans my-auto text-slate-100/40">
+							Reflect Flow
+						</h1>
+					</div>
+				</div>
+			</div>
+			<div className="row-span-11">
+				<div className="grid grid-cols-8 gap-1 h-full">
+					<div>
+						<Tabs />
+					</div>
+					<div className="flex flex-col items-center col-span-7 bg-gray-800">
+						Content
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
